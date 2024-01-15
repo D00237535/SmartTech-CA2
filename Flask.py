fc78b1d1c7964ea5ba6026c29d110e1c7104054b
Kevin Daly <kevin.daly02@outlook.com>
2024-01-15 21:33:52 +0000

    Updated Delta_Model, and updated the flask and IPYNB for the final time

--- Flask.py
+++ Flask.py
@@ -50,7 +50,8 @@ def connect(sid, environ):
 
 if __name__ == '__main__':
     try:
-        model = load_model('Delta_model.h5')
+        model = load_model('Track1_model.h5')
+        #model = load_model('Track2UpToBridge_model.h5')
         app = socketio.Middleware(sio, app)
         eventlet.wsgi.server(listen_socket, app)
     except Exception as ex:
